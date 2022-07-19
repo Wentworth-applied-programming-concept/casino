52df310317fc05fc4d53f91d93fd6b9710a5c7a1
Thomas Rice <ricet3@wit.edu>
2022-07-18 20:08:18 -0400

    Update craps.py
    First completed draft of craps. Untested as of right now.

--- src/games/craps.py
+++ src/games/craps.py
@@ -2,9 +2,11 @@ from src.core.casino import player, admin
 import random
 
 class craps:
+
     def __init__(self):
         self.player = player()
         self.admin = admin()
+        self.flag = 0
 
         run = False
         while not run:
@@ -13,11 +15,11 @@ class craps:
 
             if self.player.checkLogin(userID, pword):
                 run = True
-                self.uid = userID                           #why is this not self.player.uid?
+                self.uid = userID                                   #why is this not self.player.uid?
             else:
                 print("Login invalid, please try again")
         while run:
-            userInput = input("Would you like to play craps? (y/n): ")
+            userInput = input("Would you like to play a round of craps? (y/n): ")
 
             if userInput == 'y':
                 #self.admin.addWinnings(self.uid, -100)             #no buy-in for craps
@@ -28,46 +30,86 @@ class craps:
             else:
                 quit()
     
-
     def playGame(self):
-        '''Starts the game. TODO: return money'''
+        '''Starts the game.'''
         print("Welcome to Craps! Let's do our come-out roll.")
-        winnings += self.comeOut()
+        winnings += self.roundStart()
         return winnings
-          
-    def comeOut(self):
-        '''This function starts the round.'''
-        amtBet = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        amtBet = self.bet(self.player.getWinnings(self.uid), amtBet)
-        d1 = random.randint(1,6)
-        d2 = random.randint(1,6)
-        roll = d1+d2
-        while(1):
-            if roll in (7, 11):
-                # win chips bet on 4-11 (numbers and pass)    #THESE ARE SO F**KIN CONFUSING YOU'RE JUST BETTING ON DICE WHY IS IT THIS COMPLICATED
-                pass # remove later
-            elif roll in (2, 3, 12):
-                # win chips bet on all except 7/11 , lose chips bet on 7/11
-                pass # remove later
-            else:
-                # point routine
-                pass # remove later
     
+    def roundStart(self):
+        '''This function starts the round.'''
+        amt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.setFlag(0)
+        amt = self.bet(self.player.getWinnings(self.uid), amt)
+        roll = roll()
+        while(1):
+            if roll in (7, 11): #pass
+                # win chips bet on 7, 11 : lose on 2, 3, 12
+                winnings = amt[5] + amt[9] - amt[0] - amt[1] - amt[10]
+                print(f"You won ${winnings}! ")
+                return winnings
+            elif roll in (2, 3, 12): #dont pass
+                # win chips bet on 2, 3, 12 : lose on 7, 11
+                winnings = amt[0] + amt[1] + amt[10] - amt[5] - amt[9]
+                print(f"You won ${winnings}! ")
+                return winnings
+            else:
+                self.setFlag(1)
+                winnings = self.point(roll, amt)
+                print(f"You won ${winnings}! ")
+                return winnings
+
+    def getFlag(self):
+        return self.flag
+
+    def setFlag(self, state):
+        self.flag = state
+
+    def point(self, p, amt):
+        '''Point routine.'''
+        while(1):
+            print(f"Point established on {p}. ")
+            amt = self.bet(self.player.getWinnings(self.uid), amt)
+            roll = roll()
+            if roll in (7, 11): #lose don't passes, win passes, break
+                winnings = amt[5] + amt[9] - amt[0] - amt[1] - amt[10]
+                return winnings
+            elif roll in (2, 3, 12): #lose passes, win don't passes, continue   
+                winnings = amt[0] + amt[1] + amt[10] - amt[5] - amt[9]
+                amt[0], amt[1], amt[10], amt[5], amt[9] = 0
+                continue
+            elif roll == p: # win point, break
+                winnings = amt[p]
+                return winnings
+            else: # win whatever u bet on, continue
+                winnings = amt[roll-2]
+                amt[roll-2] = 0
+                continue
+
     def bet(self, balance, amt):
-        '''This function allows the user to place multiple bets.'''
+        '''This function allows the user to place multiple bets. TODO: add a flag to pick which mode to run.'''
         #amt.indexOf(i) = bet on the number i+2
         num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         while(1):
-            while(1):
-                i = input("Which number will you bet on? (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12): ")
-                if i in num:
-                    break
-                else:
-                    print("Invalid bet.")
+            if self.flag == 1: # point bets
+                while(1):
+                    i = input("Which number will you bet on? (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12): ")
+                    if i in num:
+                        break
+                    else:
+                        print("Invalid bet.")
+            elif self.flag == 0:
+                while(1):
+                    i = input("Which number will you bet on? (2, 3, 7, 11, 12): ")
+                    if i in (2, 3, 7, 11, 12):
+                        break
+                    else:
+                        print("Invalid bet.")
+
             while(1):
                 print(f"Available balance: {balance}")
                 temp = input("How much will you bet? ")
-                if temp <= balance:
+                if temp <= balance | temp >= 0:
                     amt[i-2] += temp
                     break
                 else:
@@ -77,3 +119,12 @@ class craps:
             else:
                 break
         return amt
+
+    def roll(self):
+        '''This function rolls the dice. Returns the sum.'''
+        _ = input("Press enter to roll the dice. ")
+        d1 = random.randint(1,6)
+        d2 = random.randint(1,6)
+        total = d1 + d2
+        print(f"[{d1}] + [{d2}] = [{total}]")
+        return total
