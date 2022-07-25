fb88751deb261d5cb2cd6705bdc94bf966e6d44a
Danastor <danastora@wit.edu>
2022-07-24 22:40:48 -0400

    slots fully integrated

--- src/games/slots.py
+++ src/games/slots.py
@@ -1,3 +1,4 @@
+from matplotlib.style import available
 from src.core.casino import player, admin
 from src.ui.ui import player as ui
 
@@ -9,21 +10,13 @@ import time
 from PIL import ImageTk, Image
 
 
-
-
-
-
-
 root = Tk()
 root.geometry("600x500")
-#root.iconbitmap('./slotAssets/images/slotsLogo.ico')
+root.iconbitmap('src/games/slotAssets/images/slotsLogo.ico')
 root.title("Casino Slots")
 
-
-
-
 pygame.mixer.init()
-#winSound=pygame.mixer.Sound("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/sounds/Winning.MP3")
+winSound=pygame.mixer.Sound("src/games/slotAssets/sounds/Winning.MP3")
 
 class Player():
     def __init__(self, name, availableAmt):
@@ -109,10 +102,10 @@ class Card():
     
 
 
-Cards=[Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot_cherry.jpg",1),Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot_diamond.jpg",2),
-        Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot_watermelon.jpg",3), Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot7pic.png",4),
-        Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slotJackpot.jpg",5),Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot_lemon.jpg",6), 
-        Card("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/slot_bar.jpg",8)]
+Cards=[Card("src/games/slotAssets/images/slot_cherry.jpg",1),Card("src/games/slotAssets/images/slot_diamond.jpg",2),
+        Card("src/games/slotAssets/images/slot_watermelon.jpg",3), Card("src/games/slotAssets/images/slot7pic.png",4),
+        Card("src/games/slotAssets/images/slotJackpot.jpg",5),Card("src/games/slotAssets/images/slot_lemon.jpg",6), 
+        Card("src/games/slotAssets/images/slot_bar.jpg",8)]
 
 
 global randNums   
@@ -125,8 +118,8 @@ def spin():
     global randNums, boardStatus
     random.shuffle(randNums)
     random.shuffle(Cards)
-    #krankSound=pygame.mixer.Sound("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/sounds/Krank.MP3")
-    #krankSound.play(loops=0)
+    krankSound=pygame.mixer.Sound("src/games/slotAssets/sounds/Krank.MP3")
+    krankSound.play(loops=0)
     
     for i in range(0,9):
         mainMenuCanvas.itemconfig(canvasCards[i], image=Cards[randNums[i]].image)
@@ -150,12 +143,13 @@ def decreaseBet():
     else:
         messagebox.showinfo("WARNING", "Bet amount has to be greater than 0")
 
-
-
+def exitGame():
+    root.destroy()
+    
 
 def startGame():
     global mainMenuCanvas
-    mainMenuCanvas = Canvas(root, width=400, height=400) #create canvas
+    mainMenuCanvas = Canvas(root, width=400, height=400) 
     mainMenuCanvas.pack(fill="both", expand=True)
      
     if(Player1.availableAmt>=0):
@@ -164,23 +158,26 @@ def startGame():
         global canvasAvailableAmt, imgBottom, topImg, betText
         global canvasCards
         
-        #backgrdMusic=pygame.mixer.Sound("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/sounds/background.MP3")
-        #backgrdMusic.play(loops=10)
+        backgrdMusic=pygame.mixer.Sound("src/games/slotAssets/sounds/background.MP3")
+        backgrdMusic.play(loops=10)
         mainMenuCanvas.create_text(590, 10, anchor ="ne", text=Player1.name,font=('Helvetica','12'))
         canvasAvailableAmt=mainMenuCanvas.create_text(590, 30, anchor ="ne", text=f"Available Cash: ${Player1.availableAmt}",font=('Helvetica','11'))
         spinButton = Button(root, text="Spin",font=('Helvetica','8', 'bold'),height=5, width=3, command=spin, borderwidth=2)
         mainMenuCanvas.create_window(330, 340, anchor="nw",window=spinButton)
         
         mainMenuCanvas.create_text(570, 400, anchor ="ne", text="Bet Amount:",font=('Helvetica','12'))
-        betPlusButton=Button(root, text="+",font=('Helvetica','8', 'bold'),height=1,bg="#2EFF2E",fg="white", width=3, command=increaseBet, borderwidth=1)
-        betminusButton=Button(root, text="-",font=('Helvetica','8', 'bold'),height=1, width=3, command=decreaseBet, borderwidth=1)
+        betPlusButton=Button(root, text="+",font=('Helvetica','8', 'bold'),height=1,bg="#2EFF2E",fg="black", width=3, command=increaseBet, borderwidth=1)
+        betminusButton=Button(root, text="-",font=('Helvetica','8', 'bold'),height=1, bg="red",fg="black",width=3, command=decreaseBet, borderwidth=1)
         mainMenuCanvas.create_window(580, 440, anchor="center",window=betPlusButton)
         mainMenuCanvas.create_window(540, 440, anchor="center",window=betminusButton)
         betText=mainMenuCanvas.create_text(570, 470, anchor ="ne", text=f"${Player1.betAmount}",font=('Helvetica','12'))
         
-        bottomload=Image.open("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/bottom.png").resize((120,85))
+        exitButton=Button(root, text="Exit",font=('Helvetica','8', 'bold'),height=1, width=3, command=exitGame)
+        mainMenuCanvas.create_window(30, 470, anchor="center",window=exitButton)
+        
+        bottomload=Image.open("src/games/slotAssets/images/bottom.png").resize((120,85))
         imgBottom= ImageTk.PhotoImage(image=bottomload)
-        topImg=ImageTk.PhotoImage(image=Image.open("C:/Users/ibrahima1/Desktop/casino/src/games/slotAssets/images/machine.png").resize((200,200)))
+        topImg=ImageTk.PhotoImage(image=Image.open("src/games/slotAssets/images/machine.png").resize((200,200)))
         canvasCards=[]
         mainMenuCanvas.create_image(265,383, anchor="center", image=imgBottom)
         mainMenuCanvas.create_image(265, 130,anchor="center", image=topImg)
@@ -195,29 +192,36 @@ def startGame():
         canvasCards.append(mainMenuCanvas.create_image(300, 260, anchor="nw",image=Cards[randNums[8]].image))
             
     else: 
-        messagebox.showinfo("Warning", "Available amount can't be empty")
+        messagebox.showinfo("Warning", "Not enough money available")
 
     root.mainloop() 
 
 
-    
-    
-
 class slots:
-     def __init__(self, uid, difficulty):
+    def __init__(self, uid, difficulty):
         self.player = player()
         self.admin = admin()
         self.uid = uid
         self.difficulty = difficulty
-
-        run = True
+        self.winnings=0
+        self.cost=0
+        
         global Player1
         Player1=Player(self.uid, self.player.getWinnings(self.uid))
-        while run:
-            startGame()
+        
+        
+        startGame()
+        if (Player1.availableAmt>self.player.getWinnings(self.uid)):
+            self.winnings= Player1.availableAmt-self.player.getWinnings(self.uid)
+        elif(Player1.availableAmt<self.player.getWinnings(self.uid)):
+            self.cost=self.player.getWinnings(self.uid)-Player1.availableAmt
             
-
-
+        self.admin.addGame('slots', self.uid,self.cost,self.winnings)
+        ui(self.uid)
+        
+            
+  
+    
 if __name__ == '__main__':
     slots()
 
